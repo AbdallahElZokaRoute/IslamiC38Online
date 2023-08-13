a547a7d7446ff92897a2bc11275077e72d844953
MuhammedRefaatMetwally <114253447+MuhammedRefaatMetwally@users.noreply.github.com>
2023-08-13 12:44:28 +0300

    Revert "features/develop/Muhammed_Refaat/Tasbeh"

---
+++ app/src/main/java/com/route/isalmic38online/fragments/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.route.isalmic38online.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.route.isalmic38online.R
+
+// Deprecated -> Will be removed in future Releases .
+
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/route/isalmic38online/ui/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.route.isalmic38online.ui
-
-class Constants {
-    companion object{
-       const val SURAH_NAME_KEY = "surah_name"
-        const val SURAH_NUMBER_KEY = "surah_num"
-    }
-}
