45b618e6b18b27427485d136fc9ba322cddfbd58
mohannadahmed00 <mohannad199344@gmail.com>
2023-08-16 16:16:06 +0200

    sebha is handled

--- app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.route.isalmic38online.R
 import com.route.isalmic38online.databinding.FragmentTasbehBinding
 
 
 class TasbehFragment : Fragment() {
 
-    lateinit var binding : FragmentTasbehBinding
-    var counter = 0
+    private lateinit var binding : FragmentTasbehBinding
+
+    private val tasbehList = listOf(
+        "سبحان الله",
+        "الحمد لله",
+        "الله أكبر",
+        "لا حول ولا قوة إلا بالله",
+        "لا إله إلا الله"
+    )
+    private var numOfTasbeh = 0
+    private var indexOfTasbeh = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,9 +31,20 @@ class TasbehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tasbehSubhanText.text = tasbehList[indexOfTasbeh]
+        binding.tasbehCounterText.text = numOfTasbeh.toString()
         binding.tasbehSubhanCardView.setOnClickListener {
-            ++counter
-            binding.tasbehCounterText.text =  counter.toString()
+
+            numOfTasbeh++
+            if (numOfTasbeh > 33) {
+                numOfTasbeh = 0
+                indexOfTasbeh++
+                if (indexOfTasbeh > tasbehList.size - 1) {
+                    indexOfTasbeh = 0
+                }
+            }
+            binding.tasbehSubhanText.text = tasbehList[indexOfTasbeh]
+            binding.tasbehCounterText.text = numOfTasbeh.toString()
         }
     }
 
