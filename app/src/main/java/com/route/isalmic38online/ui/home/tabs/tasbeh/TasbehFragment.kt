1383a223d261288be05cc7fa3d4571d8cf1a941a
Muhammed Refaat <muhammedrefaat@gmail.com>
2023-08-17 17:35:58 +0300

    +Animation Tasbeh Fragment

--- app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.route.isalmic38online.R
 import com.route.isalmic38online.databinding.FragmentTasbehBinding
 
 
@@ -22,10 +25,14 @@ class TasbehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.tasbehSubhanCardView.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(requireActivity(),R.anim.sebha_rotate)
+            binding.tasbehBody.startAnimation(animation)
             ++counter
             binding.tasbehCounterText.text =  counter.toString()
         }
+
     }
 
 }
