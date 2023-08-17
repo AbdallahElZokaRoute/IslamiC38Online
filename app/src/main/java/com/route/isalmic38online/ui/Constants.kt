b4d151ff6066b01a6bde7b6877ecc4edba5e5e71
Muhammed Refaat <muhammedrefaat@gmail.com>
2023-08-17 19:10:45 +0300

    +Animation Tasbeh Fragment +Change Rememberance +Dynamic Text Size based on the Text

--- app/src/main/java/com/route/isalmic38online/ui/Constants.kt
+++ app/src/main/java/com/route/isalmic38online/ui/Constants.kt
@@ -7,5 +7,9 @@ class Constants {
         const val HADITH_TITLE = "hadith_title"
         const val HADITH_CONTENT = "hadith_content"
         const val NIGHT_MODE = "night_mode"
+        const val SUBHAN_ALLAH = "سبحان الله"
+        const val EL_HAMDULLAH = "الحمد لله"
+        const val ALLAHAKBUR = "الله أكبر"
+        const val ELKETMA = "لا اله الا  الله وحده لا شريك له الملك و له الحمد و هو على كل شيء قدير"
     }
 }
--- app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/route/isalmic38online/ui/home/tabs/tasbeh/TasbehFragment.kt
@@ -9,6 +9,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import com.route.isalmic38online.R
 import com.route.isalmic38online.databinding.FragmentTasbehBinding
+import com.route.isalmic38online.ui.Constants
 
 
 class TasbehFragment : Fragment() {
@@ -25,14 +26,39 @@ class TasbehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+         binding.tasbehSubhanText.text = Constants.SUBHAN_ALLAH
         binding.tasbehSubhanCardView.setOnClickListener {
-            val animation = AnimationUtils.loadAnimation(requireActivity(),R.anim.sebha_rotate)
-            binding.tasbehBody.startAnimation(animation)
+            //val animation = AnimationUtils.loadAnimation(requireActivity(),R.anim.sebha_rotate)
+            binding.tasbehBody.rotation = binding.tasbehBody.rotation+5
             ++counter
             binding.tasbehCounterText.text =  counter.toString()
+
+            changeRemembrance()
         }
 
+    }
+
+    private fun changeRemembrance() {
+        if(counter == 33 && binding.tasbehSubhanText.text == Constants.SUBHAN_ALLAH){
+            binding.tasbehSubhanText.text = Constants.EL_HAMDULLAH
+            restCounter()
+        }else if(counter == 33 && binding.tasbehSubhanText.text == Constants.EL_HAMDULLAH){
+            binding.tasbehSubhanText.text = Constants.ALLAHAKBUR
+            restCounter()
+        }else if(counter == 33 && binding.tasbehSubhanText.text == Constants.ALLAHAKBUR){
+            binding.tasbehSubhanText.textSize = 18F
+            binding.tasbehSubhanText.text = Constants.ELKETMA
+            restCounter()
+        }else if(counter == 33 && binding.tasbehSubhanText.text == Constants.ELKETMA){
+            binding.tasbehSubhanText.textSize = 36F
+            binding.tasbehSubhanText.text = Constants.SUBHAN_ALLAH
+            restCounter()
+        }
+    }
+
+    fun restCounter(){
+        counter=0
+        binding.tasbehCounterText.text =  counter.toString()
     }
 
 }
